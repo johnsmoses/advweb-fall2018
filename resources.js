003c9edbfcc68567a216f1b408262145ff1737fb
johnsmoses <jmoses323@gmail.com>
2018-09-16 20:58:55 -0400

    Update resources.js

--- resources.js
+++ resources.js
@@ -7,14 +7,14 @@ $(document).ready(function () {
    
     $.ajax({
     type: "GET",
-    url: "https://johnsmoses.github.io/advweb-fall2018/google-books-search.json",
+    url: "https://johnsmoses.github.io/advweb-fall2018/google-books-book.json",
     success: function(result)
     {
     console.log(result);
     var output="<table><thead><tr><th>Name</th><th>Provider</th><th>URL</th></thead><tbody>";
     for (var i in result)
     {
-    output += "<tr><td>" + result[i].items[i].kind[i] + "</td><td>" + result[i].items[i].kind[i] + "</td><td>" + result[i].items[i].kind[i] + "</td></tr>";    
+    output += "<tr><td>" + result[i].volumeInfo.title + "</td><td>" + result[i].volumeInfo.title + "</td><td>" + result[i].volumeInfo.title + "</td></tr>";    
     }
     output+="</tbody></table>";
    
